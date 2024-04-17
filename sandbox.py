65c19908562fffcd9d58e984f499296a76bf95c3
MK <m.kotov@qasquad.com>
2024-04-17 16:54:57 +0700

    Russian field of experiments

--- sandbox.py
+++ sandbox.py
@@ -6,4 +6,4 @@ from pages import locators
 
 //*[text()="№1024234"] #не нужен. Заменяется на https://app-monolith.mylgc.ru/bitrix/admin/sale_order_view.php?amp%3Bfilter=Y&%3Bset_filter=Y&lang=ru&ID=1024233
 select = Select(driver.find_element_by_id(By.CSS_SELECTOR, "#STATUS_ID"))
-select.select_by_value("NI")
+#select.select_by_value("NI")
