a7d4b0eadc03c54bf3bf4a82e80d00abe652c5d1
mik-kotov <mikhail.al.kotov@gmail.com>
2024-08-15 18:42:33 +0700

    - test bitrix

--- Tests/test_loyalty.py
+++ Tests/test_loyalty.py
@@ -14,22 +14,39 @@ def test_bitr_ops(driver):
     bitrix_ops = Bitrix(driver)
     bitrix_ops.authorization()
     bitrix_ops.open('https://app-monolith.mylgc.ru/bitrix/admin/sale_order_edit.php?ID=1302484&lang=ru')
+    # allure.attach(driver.get_screenshot_as_png(), name="Скрин",
+    #               attachment_type=allure.attachment_type.PNG)
+    # element = driver.find_element(By.CSS_SELECTOR, '.adm-s-gray-title')
+    # driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    # allure.attach(driver.get_screenshot_as_png(), name="Скрин",
+    #               attachment_type=allure.attachment_type.PNG)
+    # time.sleep(5)
+    # element_a = driver.find_element(By.CSS_SELECTOR, '.adm-s-order-item-title-icon')
+    # bitrix_ops.click(element_a)
+    # time.sleep(1)
+    # allure.attach(driver.get_screenshot_as_png(), name="Скрин",
+    #               attachment_type=allure.attachment_type.PNG)
+    # element_b = driver.find_element(By.CSS_SELECTOR, '.bx-core-popup-menu-item-text')
+    # bitrix_ops.click(element_b)
+    # allure.attach(driver.get_screenshot_as_png(), name="Скрин",
+    #               attachment_type=allure.attachment_type.PNG)
+    # bitrix_ops.open('https://app-monolith.mylgc.ru/bitrix/admin/sale_order_edit.php?ID=1302484&lang=ru')
     allure.attach(driver.get_screenshot_as_png(), name="Скрин",
                   attachment_type=allure.attachment_type.PNG)
+    time.sleep(2)
     element = driver.find_element(By.CSS_SELECTOR, '.adm-s-gray-title')
     driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    time.sleep(2)
     allure.attach(driver.get_screenshot_as_png(), name="Скрин",
                   attachment_type=allure.attachment_type.PNG)
-    time.sleep(5)
-    element_a = driver.find_element(By.CSS_SELECTOR, '.adm-s-order-item-title-icon')
-    bitrix_ops.click(element_a)
+    bitrix_ops.browser.execute_script("document.querySelector('.adm-s-order-item-title-icon').click()")
     time.sleep(1)
     allure.attach(driver.get_screenshot_as_png(), name="Скрин",
                   attachment_type=allure.attachment_type.PNG)
-    element_b = driver.find_element(By.CSS_SELECTOR, '.bx-core-popup-menu-item-text')
-    bitrix_ops.click(element_b)
+    bitrix_ops.browser.execute_script("document.querySelector('.bx-core-popup-menu-item-text').click()")
     allure.attach(driver.get_screenshot_as_png(), name="Скрин",
                   attachment_type=allure.attachment_type.PNG)
+
 
 @allure.issue("https://jira.pochtavip.com/secure/Tests.jspa#/testCase/LGC-T2332", "LGC-T2332")
 @allure.feature("Доставлен")
